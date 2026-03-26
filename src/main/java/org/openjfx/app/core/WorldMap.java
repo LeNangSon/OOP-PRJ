071f9ec7075cec1d27e15abeb96257f4cd695d99
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-03-26 23:19:55 +0700

    Bản đồ

---
+++ src/main/java/org/openjfx/app/core/WorldMap.java
@@ -0,0 +1,50 @@
+package org.openjfx.app.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openjfx.app.entities.base.Entity;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class WorldMap {
+    private final double width;
+    private final double height;
+    private final List<Entity> entities;
+
+    public WorldMap(double width, double height) {
+        this.width = width;
+        this.height = height;
+        this.entities = new ArrayList<>();//thêm các con vật và vật tĩnh
+    }
+
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+    }
+
+    public void update(double dt) {
+        for (Entity entity : entities) {
+            entity.update(dt);
+        }
+    }
+
+    public void render(GraphicsContext gc) {
+        //Vẽ nền Map
+        gc.clearRect(0, 0, width, height); 
+        //chèn ảnh vào tọa độ của vật
+        for (Entity entity : entities) {
+            renderEntityWithImage(gc, entity);
+        }
+    }
+
+    private void renderEntityWithImage(GraphicsContext gc, Entity entity) {
+    // 1. Tách chuỗi từ toString() để lấy tên lớp
+    String[] parts = entity.toString().split("\\{");
+    String imagePath = parts[0]; 
+    // 2. Load ảnh trực tiếp từ chuỗi đã tách
+    Image img = new Image(getClass().getResourceAsStream("/" + imagePath));
+    // 3. Vẽ ảnh lên Canvas
+        gc.drawImage(img,entity.getPosition().x,entity.getPosition().y,entity.getSize(),entity.getSize());
+}
+}
