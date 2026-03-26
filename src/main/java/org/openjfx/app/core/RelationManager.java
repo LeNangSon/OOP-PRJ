d511cd807fdf943a338a7d60398b03e0ba9c7b5e
Hồ Xuân Phương <phuongit.hoxuan@gmail.com>
2026-03-27 02:47:53 +0700

    Basic of FleeStrategy

--- src/main/java/org/openjfx/app/core/RelationManager.java
+++ src/main/java/org/openjfx/app/core/RelationManager.java
@@ -1,21 +1,40 @@
 package org.openjfx.app.core;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RelationManager {
 
-    static {
-        
-    }
+   private static final Map<EntityType, List<EntityType>> threat = new HashMap<>();
+   static{
+    threat.put(EntityType.RABBIT, Arrays.asList(EntityType.WOLF, EntityType.BEAR));
+    threat.put(EntityType.FISH, Arrays.asList(EntityType.BEAR));
+    threat.put(EntityType.FRUIT, Arrays.asList(EntityType.BEAR, EntityType.RABBIT));
+    threat.put(EntityType.GRASS, Arrays.asList(EntityType.RABBIT, EntityType.ELEPHANT));
+    threat.put(EntityType.ALGAE, Arrays.asList(EntityType.FISH));
+   }
 
     public static boolean isScaredOf(EntityType subject, EntityType target) {
-        return false; 
+        return false;
     }
 
     public static boolean isPrey(EntityType subject, EntityType target) {
-        
+        if (threat.containsKey(subject) ){
+            for (EntityType animal : threat.get(subject)){
+                if (animal == target){
+                    return true;
+                }
+            }
+
+        }
         return false;
+        
     }
 
     public static boolean isEdiblePlant(EntityType subject, EntityType target) {
         return false;
     }
+
+    
 }
