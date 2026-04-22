9f8a5b76b9b67c88c8090fdf92ea20dc5dffdcd8
lenangson <Son.LN2417019@sis.hust.edu.vn>
2026-04-22 18:26:59 +0700

    Naive A* pathfinding

--- src/main/java/org/openjfx/app/core/strategies/SeekWaterStrategy.java
+++ src/main/java/org/openjfx/app/core/strategies/SeekWaterStrategy.java
@@ -18,18 +18,23 @@ public class SeekWaterStrategy implements MoveStrategy {
     public void updateVelocity(LivingEntity owner, List<Entity> neighbors, double dt, WorldMap world){
         Vector2D currentPos = owner.getPosition();
 
+        // Nếu vị trí đang đứng là nước --> Uống
         if (world.getTerrainAt(currentPos) == TerrainType.WATER) {
             owner.setVelocity(new Vector2D(0, 0));
             owner.drink(dt);
             return;
         }
 
+        // Tìm nguồn nước từ vị trí hiện tại
         Vector2D nearestWater = world.findNearestTerrainPosition(currentPos, TerrainType.WATER);
+        // Nếu không tìm thấy thì đi lang thang
         if (nearestWater == null) {
             this.searchWander.updateVelocity(owner, neighbors, dt, world);
             return;
         }
 
+
+        // Đã tìm thấy nguồn nước (thuật toán hiện tại chỉ là đang đi thẳng đến nguồn nước đó)
         double distanceToWater = currentPos.distance(nearestWater);
         if(distanceToWater > 5){
             Vector2D direct = currentPos.directionTo(nearestWater);
